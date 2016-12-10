c20c78bac887773867dd7c514e6f8153bc089487
Bruno Giovanini <bsgiovanini@gmail.com>
2016-12-10 17:05:55 -0200

    some advances

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,19 +10,33 @@ class Home extends Component {
 
       	<section className="posts">
   
-  			<section className="post banner">
-			    <h1 className="post-title">I build <em>scalable</em>, <em>maintainable</em> and <em>secure</em> enterprise web applications.</h1>
-    			<p>for <em>agencies</em>, <em>bluechips</em>, <em>start-ups</em> and sometimes, <em>myself</em>. <a href="#"> Find out More</a></p>
+  			<div id="who-i-am" className="row">
 
-			</section>
+
+  				<div className="col-xs-12 title">	
+  					<h2>About Me</h2>
+  				</div>
+
+  				<div className="col-xs-12">	
+  					<img src="me.jpg" className="img-responsive img-circle pic" alt="Me" width="200" height="200" />
+  				</div>
+  				
+  				<div className="col-xs-12">
+  					<h2>I build <em>scalable</em>, <em>maintainable</em> and <em>secure</em> enterprise web applications.</h2>
+    				<p>for <em>agencies</em>, <em>bluechips</em>, <em>start-ups</em> and sometimes, <em>myself</em>. <a href="#"> Find out More</a></p>
+  				</div>
+			    
+			</div>
   
-		  	<section className="post clearfix">
-
-				<h2>Technologies I build with:</h2>
-
-				<article className="skill-item clearfix">
-					<h4>Client Side JS</h4>
-					<ul className="">
+		  	<div id="skills" className="row">
+		  		<div className="col-xs-12">
+  					<h2>Technologies I build with:</h2>
+  				</div>
+		  	</div>
+		  	<div className="row">		
+		  		<div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+		  			<h4>Client Side JS</h4>
+					<ul>
 						<li><em>Backbone</em></li>
 						<li><em><b>*</b><b>*</b>React</em>
 							<ul>
@@ -30,13 +44,12 @@ class Home extends Component {
 								<li><em>Flux</em></li>
 							</ul>
 						</li>
-						<li><em>D3</em></li>
+						<li>Elm</li>
+						
 					</ul>
-				</article>
-
-
-				<article className="skill-item clearfix">
-					<h4>Server Side JS</h4>
+		  		</div>
+		  		<div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+		  			<h4>Server Side JS</h4>
 					<ul>
 						<li><em>MongoDB</em></li>
 						<li><em><b>*</b>Node</em>
@@ -46,51 +59,58 @@ class Home extends Component {
 							</ul>
 						</li>
 					</ul>
-				</article>
+		  		</div>
 
-				<article className="skill-item clearfix">
-					<h4>Testing</h4>
+		  		<div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+		  			<h4>Deployment</h4>
+					<ul>
+						<li><em><b>*</b>Nginx</em></li>
+						<li><em><b>*</b>Webpack</em></li>
+						<li><em>Gulp</em></li>
+						<li><em>Grunt</em></li>
+					</ul>
+		  		</div>
+
+		  		<div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+		  			<h4>Testing</h4>
 					<ul>
 						<li><em>Jasmine</em></li>
 						<li><em>Karma</em></li>
 						<li><em>Protractor</em></li>
 						<li><em>Jest</em></li>
 					</ul>
-				</article>
+		  		</div>
 
-				<article className="skill-item clearfix">
-					<h4>Deployment</h4>
-					<ul className="clearfix">
-						<li><em><b>*</b>Nginx</em></li>
-						<li><em><b>*</b>Webpack</em></li>
-						<li><em>Gulp</em></li>
-						<li><em>Grunt</em></li>
-					</ul>
-				</article>
+		  		
 
-				<div className="exclaimation">
+				<div className="col-xs-12">
 					<em><b>*</b> This site is built with these technologies. View the <a href="https://github.com/bsgiovanini/portfolio-redux-app">github repo here</a>.</em><br/>
 					<em><b>**</b> Yep, I know React can be used on the server side too. This site is Isomorphical rendered.</em>
 				</div>
 
-  			</section>
+  			</div>
 
 
-  			<section className="post clearfix">
+  			<div className="row">
 
-				<h2>Companies I have worked with:</h2>
+  				<div className="col-xs-12">
+  					<h2>Companies I have worked with:</h2>
+  				</div>
+  				<div className="col-xs-12">
+  					<ul className="clients">
+						<li className="petro-logo">
+							<h3>Petrobras</h3>
 
-				<ul className="clients">
-					<li className="goldmans-logo">Petrobras</li>
-					<li className="orange-logo">Concrete Solutions</li>
-					<li className="tesco-logo">Anc</li>
-					<li className="timeout-logo">Timeout</li>
-					<li className="wiley-logo">John Wiley and Sons</li>
-					<li className="covestor-logo">Covestor</li>
-					<li className="shipserv-logo">Shipserv</li>
-				</ul>
+						</li>
 
-			</section>
+						<li className="cs-logo">
+							<h3>Concrete Solutions</h3>
+						</li>
+					</ul>
+  				</div>
+				
+
+			</div>
 
 			<Banner />
 
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,7 +6,6 @@ class Header extends Component {
     return (
 	    <header className="jumbotron jumbotron-bg-lg jumbotron-bg-md jumbotron-bg-sm jumbotron-bg-xs">
 			<nav className="navbar navbar-default">
-			  <div className="-fluid">
 			    {/*Brand and toggle get grouped for better mobile display*/}
 			    <div className="navbar-header">
 			      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -15,48 +14,24 @@ class Header extends Component {
 			        <span className="icon-bar"></span>
 			        <span className="icon-bar"></span>
 			      </button>
-			      <a className="navbar-brand" href="#">Me</a>
+			      <a className="navbar-brand" href="#"><span className="glyphicon glyphicon-home" aria-hidden="true"></span></a>
 			    </div>
 
 			    {/* Collect the nav links, forms, and other content for toggling*/}
 			    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			      <ul className="nav navbar-nav">
-			        <li><a href="#">Who I am <span className="sr-only">(current)</span></a></li>
-			        <li><a href="#">Skills</a></li>
-			        <li className="dropdown">
-			          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-			          <ul className="dropdown-menu">
-			            <li><a href="#">Action</a></li>
-			            <li><a href="#">Another action</a></li>
-			            <li><a href="#">Something else here</a></li>
-			            <li role="separator" className="divider"></li>
-			            <li><a href="#">Separated link</a></li>
-			            <li role="separator" className="divider"></li>
-			            <li><a href="#">One more separated link</a></li>
-			          </ul>
-			        </li>
+			        <li><a href="#"><span className="glyphicon glyphicon-user" aria-hidden="true"></span><span className="hidden-sm">About Me</span></a></li>
+			        <li><a href="#"><span className="glyphicon glyphicon-book" aria-hidden="true"></span><span className="hidden-sm">My Skills</span></a></li>
+			        <li><a href="#"><span className="glyphicon glyphicon-cd" aria-hidden="true"></span><span className="hidden-sm">Who I Worked With</span></a></li>
+			        <li><a href="#"><span className="glyphicon glyphicon-education" aria-hidden="true"></span><span className="hidden-sm">My Education</span></a></li>
+			        <li><a href="#"><span className="fa fa-linkedin-square fa-lg" aria-hidden="true"></span><span className="hidden-sm">My Linkedin</span></a></li>
+			        <li><a href="#"><span className="glyphicon glyphicon-cloud" aria-hidden="true"></span><span className="hidden-sm">Repositories</span></a></li>
 			      </ul>
-			      <form className="navbar-form navbar-left">
-			        <div className="form-group">
-			          <input type="text" className="form-control" placeholder="Search"></input>
-			        </div>
-			        <button type="submit" className="btn btn-default">Submit</button>
-			      </form>
 			      <ul className="nav navbar-nav navbar-right">
-			        <li><a href="#">Link</a></li>
-			        <li className="dropdown">
-			          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-			          <ul className="dropdown-menu">
-			            <li><a href="#">Action</a></li>
-			            <li><a href="#">Another action</a></li>
-			            <li><a href="#">Something else here</a></li>
-			            <li role="separator" className="divider"></li>
-			            <li><a href="#">Separated link</a></li>
-			          </ul>
-			        </li>
+			        <li><a href="#"><span className="glyphicon glyphicon-envelope" aria-hidden="true"></span><span className="hidden-sm">Contact</span></a></li>
 			      </ul>
 			    </div>{/*navbar-collapse*/}
-			  </div>{/*container-fluid*/}
+			  
 			</nav>
 			<div className="persona">
 				<h1>Bruno S. Giovanini</h1>
@@ -65,6 +40,8 @@ class Header extends Component {
 			</div>
 			<h4 className="dictation">"There is no personal growth, no change in your life if you never try"</h4>
 		</header>
+		
+
     );
   }
 }
