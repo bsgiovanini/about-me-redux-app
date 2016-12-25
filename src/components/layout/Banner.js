b9ddb2c52e65d428d9fe4dea7933acbce9d188ed
Bruno Giovanini <bsgiovanini@gmail.com>
2016-12-25 20:45:55 -0200

    resize ghiphon

--- src/components/layout/Banner.js
+++ src/components/layout/Banner.js
@@ -8,19 +8,19 @@ class Banner extends Component {
 	    <footer className="contact-banner">
       		<ul className="list-inline">
       			<li className="social-linkedin">
-					<a href="https://linkedin.com/in/bsgiovanini" target="_blank"><i className="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>
+					<a href="https://linkedin.com/in/bsgiovanini" target="_blank"><i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
 				</li>
 				<li className="social-github">
-					<a href="https://github.com/bsgiovanini" target="_blank"><i className="fa fa-github-square fa-3x" aria-hidden="true"></i></a>
+					<a href="https://github.com/bsgiovanini" target="_blank"><i className="fa fa-github-square fa-2x" aria-hidden="true"></i></a>
 				</li>
 				<li className="social-twitter">
-					<a href="https://twitter.com/bsgiovanini" target="_blank"><i className="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
+					<a href="https://twitter.com/bsgiovanini" target="_blank"><i className="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a>
 				</li>
 				<li className="social-facebook">
-					<a href="https://www.facebook.com/bsgiovanini" target="_blank"><i className="fa fa-facebook-square fa-3x" aria-hidden="true"></i></a>
+					<a href="https://www.facebook.com/bsgiovanini" target="_blank"><i className="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a>
 				</li>
 				<li className="social-google-plus">
-					<a href="https://plus.google.com/bsgiovanini" target="_blank"><i className="fa fa-google-plus-square fa-3x" aria-hidden="true"></i></a>
+					<a href="https://plus.google.com/bsgiovanini" target="_blank"><i className="fa fa-google-plus-square fa-2x" aria-hidden="true"></i></a>
 				</li>
 			</ul>
 
